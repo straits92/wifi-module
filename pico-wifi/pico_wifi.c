decfecf3008377a1aa728b3dfd037c269be3abd8
straits92 <adam.k.naas@gmail.com>
2022-04-23 14:22:57 +0200

    Implement echo of received data over UART. Also put 24 characters on Tx at each read.

--- pico-wifi/pico_wifi.c
+++ pico-wifi/pico_wifi.c
@@ -17,11 +17,10 @@
 #include <string.h>
 #include "hardware/gpio.h"
 
-#define PRINT_TO_STDOUT 1
-#define PRINT_TO_UART 0
+#define PRINT_TO_UART 1
 #define BUFFER_SIZE 512
 #define UART_ID uart0
-#define PARITY UART_PARITY_NONE
+#define PARITY    UART_PARITY_NONE
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
@@ -34,6 +33,7 @@ const char* device_off = "device=off";
 const char* wifi_connected = "WiFi connected";
 const char* generic = "Substring received from Rx by Pico:";
 char tmp_msg[BUFFER_SIZE];
+
 
 int main() {
 	// usb output
@@ -49,53 +49,51 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);   
     
-    if (PRINT_TO_STDOUT != 0) {printf("Pico script to be executed.\n");}
-    if (PRINT_TO_UART != 0) { 
-		uart_puts(UART_ID, "PICO INIT LINE 1\n");
-    }
+	uart_puts(UART_ID, "PICO INIT LINE 1\n");
 
     while (1) {
 		if (uart_is_readable(UART_ID)) {
-		    
-		    int i = 0;
-		    while (uart_is_readable(UART_ID)) {				
-			    // read char and skip newline
-			    char ch = uart_getc(UART_ID);
-			    if(ch!='\n' && ch!='\r') {
-				    tmp_msg[i] = ch;
-				    i++;
-			    } else {
-				    tmp_msg[i] = '\0'; 
-				    break;
-			    }
-		    }
+			
+			int i = 0;
+			while (uart_is_readable(UART_ID)) {				
+				// read char and skip newline
+				char ch = uart_getc(UART_ID);
+				if(ch!='\n' && ch!='\r') {
+					tmp_msg[i] = ch;
+					i++;
+				} else {
+					tmp_msg[i] = '\0'; 
+					break;
+				}
 
-			    
-			if (PRINT_TO_STDOUT != 0) {
-				// printf("Size: [%d] |\nPayload:\n", i);
-				printf("\n");
-				printf(tmp_msg);
-				printf("\n");
-			}		    
-		    
-	    
-		    // only handle nonzero payloads which match template
-		    if ( i != 0 ) {
-			    if (strstr(tmp_msg, device_on) != NULL) {
-				    //device_state = 1;
-				    gpio_put(LED_PIN, 1);
-				    
-				    if (PRINT_TO_UART != 0) { uart_puts(UART_ID, " \n"); }
+			}
 
-			    }
-			    if (strstr(tmp_msg, device_off) != NULL) {
-				    //device_state = 0;
-				    gpio_put(LED_PIN, 0);
-				    if (PRINT_TO_UART != 0) { uart_puts(UART_ID, " \n"); }
+			/* Even though 24 characters are put, 4*24 = 96 are received 
+			 * by the Wemos */
+			if (PRINT_TO_UART != 0) {
+				uart_puts(UART_ID, "xxxxxxxxxxxxxxxxxxxxxxxx");
+			}
+		
+			// only handle nonzero payloads
+			if ( i != 0 ) {
+				// check if msg fits on/off template
+				if (strstr(tmp_msg, device_on) != NULL) {
+					gpio_put(LED_PIN, 1);
 
-			    } 
-		    }
+				}
+				if (strstr(tmp_msg, device_off) != NULL) {
+					//device_state = 0;
+					gpio_put(LED_PIN, 0);
 
+				} 
+
+				/* this exact message is received by the Wemos, without 
+				 * duplication; but only after the  24-x string is sent
+				 * 3 out of 4 times. Why? */
+				uart_puts(UART_ID, "\nReceived: ");	
+				uart_puts(UART_ID, tmp_msg);
+				uart_putc(UART_ID, '\n');
+			}
 		} 
     }
 }
