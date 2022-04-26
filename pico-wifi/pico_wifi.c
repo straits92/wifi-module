f99e243ebf486826d5e872e1bd1c91c110e808e5
straits92 <adam.k.naas@gmail.com>
2022-04-26 23:36:16 +0200

    Fix partial reading and buffer overflow by reading char-by-char.

--- pico-wifi/pico_wifi.c
+++ pico-wifi/pico_wifi.c
@@ -17,7 +17,7 @@
 #include <string.h>
 #include "hardware/gpio.h"
 
-#define PRINT_TO_UART 1
+#define PRINT_BUFFER_TO_UART 1
 #define BUFFER_SIZE 512
 #define UART_ID uart0
 #define PARITY    UART_PARITY_NONE
@@ -36,7 +36,6 @@ char tmp_msg[BUFFER_SIZE];
 
 
 int main() {
-	// usb output
     stdio_init_all(); 
 
     // setting up UART
@@ -50,50 +49,35 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);   
     
 	uart_puts(UART_ID, "PICO INIT LINE 1\n");
+	int i = 0;
 
     while (1) {
-		if (uart_is_readable(UART_ID)) {
+    	/* uart_getc no longer in a while loop which checks if uart_is_readable, 
+    	 * because the looping is quicker than the UART transmission and might
+    	 * actually exit at uart_is_readable = false before the entire message
+    	 * is received from the Wemos. */
+		char ch = uart_getc(UART_ID);
+		if(ch!='\n' && ch!='\r') {
+			tmp_msg[i++] = ch;
+		} else {
+			tmp_msg[i] = '\0';
+
+			if (i != 0) {
+
+				/* This is also duplicated when sent. */
+				uart_puts(UART_ID, "\nPICO_ECHO: ");	
+				uart_puts(UART_ID, tmp_msg);
 			
-			int i = 0;
-			while (uart_is_readable(UART_ID)) {				
-				// read char and skip newline
-				char ch = uart_getc(UART_ID);
-				if(ch!='\n' && ch!='\r') {
-					tmp_msg[i] = ch;
-					i++;
-				} else {
-					tmp_msg[i] = '\0'; 
-					break;
-				}
-
-			}
-
-			/* Even though 24 characters are put, 4*24 = 96 are received 
-			 * by the Wemos */
-			if (PRINT_TO_UART != 0) {
-				uart_puts(UART_ID, "xxxxxxxxxxxxxxxxxxxxxxxx");
-			}
-		
-			// only handle nonzero payloads
-			if ( i != 0 ) {
 				// check if msg fits on/off template
 				if (strstr(tmp_msg, device_on) != NULL) {
 					gpio_put(LED_PIN, 1);
-
 				}
 				if (strstr(tmp_msg, device_off) != NULL) {
-					//device_state = 0;
 					gpio_put(LED_PIN, 0);
-
 				} 
-
-				/* this exact message is received by the Wemos, without 
-				 * duplication; but only after the  24-x string is sent
-				 * 3 out of 4 times. Why? */
-				uart_puts(UART_ID, "\nReceived: ");	
-				uart_puts(UART_ID, tmp_msg);
-				uart_putc(UART_ID, '\n');
 			}
+
+			i = 0;
 		} 
     }
 }
